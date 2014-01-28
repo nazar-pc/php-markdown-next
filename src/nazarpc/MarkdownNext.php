86eeaab4f070fbf78e0a5dc3973fdc3bed9ecc33
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-01-28 23:22:45 +0200

    In addition to fix for issue #6

--- src/nazarpc/MarkdownNext.php
+++ src/nazarpc/MarkdownNext.php
@@ -1188,7 +1188,7 @@ class MarkdownNext {
 				$alt_text		= $matches[2];
 				$link_id		= strtolower($matches[3]);
 				if ($link_id == '') {
-					$link_id = strtolower($alt_text); // for shortcut links like ![this][].
+					$link_id = $alt_text; // for shortcut links like ![this][].
 				}
 				$alt_text		= $this->encodeAttribute($alt_text);
 				if (isset($this->urls[$link_id])) {
